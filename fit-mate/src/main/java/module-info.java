4de2ae2b4619d1afce30d1089e00e62e1a22e380
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-26 20:39:25 +0530

    Create module-info.java

---
+++ fit-mate/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module tech.altier.fitmate {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens tech.altier.fitmate to javafx.fxml;
+    exports tech.altier.fitmate;
+}
