9c965ef6a5a560a37b67a4379ce6453c1780ad79
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-25 19:12:55 +0530

    Create index.js
    Co-Authored-By: K. N. U. RANASINGHE <113103000+knuranasinghe@users.noreply.github.com>

---
+++ frontend/fitness-web/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App/>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
