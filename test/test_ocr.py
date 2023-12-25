25942a61e281dff4c1f2d6b41dd7af6950867e24
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-25 18:43:39 +0530

    Update test_ocr.py

---
+++ test/test_ocr.py
@@ -0,0 +1,9 @@
+import cv2
+import pytesseract
+import matplotlib.pyplot as plt
+import re
+import nltk
+
+tessdata_dir_config = '--tessdata-dir "C:\\Program Files\\Tesseract-OCR\\tessdata"'
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
