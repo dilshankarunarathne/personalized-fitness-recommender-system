a6aa5ae3b949ac4d22eeb9ca93433024428aa2f2
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-25 18:47:12 +0530

    Update test_ocr.py

--- test/test_ocr.py
+++ test/test_ocr.py
@@ -7,22 +7,23 @@ import nltk
 tessdata_dir_config = '--tessdata-dir "C:\\Program Files\\Tesseract-OCR\\tessdata"'
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
+
 def process_image(img_path):
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     text = pytesseract.image_to_string(gray)
 
-    sentences = nltk.sent_tokenize(text.lower()) # Tokenize the text into sentences
-    number_regex = r'\b\d{2,3}\b' # Define the regular expression for a number
-    final_numbers = {} # number : priority
+    sentences = nltk.sent_tokenize(text.lower())  # Tokenize the text into sentences
+    number_regex = r'\b\d{2,3}\b'  # Define the regular expression for a number
+    final_numbers = {}  # number : priority
 
-    for sentence in sentences: # Iterate over the sentences
+    for sentence in sentences:  # Iterate over the sentences
         # Tokenize the sentence into words
         words = nltk.word_tokenize(sentence)
         for i in range(len(words)):
             # Check if the word is a 2 or 3 digit number
             if re.match(number_regex, words[i]):
-                final_numbers[words[i]] = 1 # gives a priority as 1
+                final_numbers[words[i]] = 1  # gives a priority as 1
 
     # Iterate over the keys of the final_numbers dictionary
     for number in final_numbers.keys():
