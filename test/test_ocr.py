cf6396e0b76eea66516a134ca1cb6272da796751
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-25 18:50:04 +0530

    Update test_ocr.py

--- test/test_ocr.py
+++ test/test_ocr.py
@@ -11,7 +11,7 @@ def ocr(img_path):
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     text = pytesseract.image_to_string(gray)
-    text = text.lower()
+    return text.lower()
 
 
 def process_text(text):
