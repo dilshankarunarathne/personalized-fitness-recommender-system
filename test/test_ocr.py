aa82933ba3d7cce4d7ff31e0af7e9d1707d5c04b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-25 18:49:53 +0530

    Update test_ocr.py

--- test/test_ocr.py
+++ test/test_ocr.py
@@ -6,10 +6,10 @@ import pytesseract
 tessdata_dir_config = '--tessdata-dir "C:\\Program Files\\Tesseract-OCR\\tessdata"'
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-img = cv2.imread(img_path)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-text = pytesseract.image_to_string(gray)
-text = text.lower()
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    text = pytesseract.image_to_string(gray)
+    text = text.lower()
 
 
 def process_text(text):
