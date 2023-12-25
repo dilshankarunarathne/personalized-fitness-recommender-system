1a8ff26b081b4dce6b8c3a29f9285aa14f6206fd
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-25 19:15:33 +0530

    Update ocr.py

--- backend/blood_report_analyzer/ocr.py
+++ backend/blood_report_analyzer/ocr.py
@@ -1,8 +1,13 @@
 import cv2
 import pytesseract
 
-# tessdata_dir_config = '--tessdata-dir "C:\\Program Files\\Tesseract-OCR\\tessdata"'
-# pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# from backend.config import get
+
+tessdata_dir_config = '--tessdata-dir "C:\\Program Files\\Tesseract-OCR\\tessdata"'
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+# tessdata_dir_config = get('tesseract', 'dir')
+# pytesseract.pytesseract.tesseract_cmd = get('tesseract', 'cmd')
 
 
 def ocr(img_path) -> str:
