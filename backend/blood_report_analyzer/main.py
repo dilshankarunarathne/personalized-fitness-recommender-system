4264d2d107c0b04446d8e294055358723a646965
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-12-25 19:25:36 +0530

    Update main.py

--- backend/blood_report_analyzer/main.py
+++ backend/blood_report_analyzer/main.py
@@ -2,7 +2,7 @@
 
 def analyze_blood_sugar_report(report_img_data): # TODO
     # read base64 str image data
-    ocr_img_base64(report_img_data)
+    img_data = ocr_img_base64(report_img_data)
     # convert to text using ocr module
 
     # process text using analyzer module
