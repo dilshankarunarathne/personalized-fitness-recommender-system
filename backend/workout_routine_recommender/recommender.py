52c26e32635ff036fd52cb050f2e5bd26d98df5a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-13 03:51:36 +0530

    Update recommender.py

--- backend/workout_routine_recommender/recommender.py
+++ backend/workout_routine_recommender/recommender.py
@@ -1,12 +1,5 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, OrdinalEncoder, Normalizer
-from sklearn.impute import SimpleImputer
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.multioutput import MultiOutputRegressor
 import joblib
+import pandas as pd
 
 # Define the categorical and numerical features
 cat_features = ['Gender']
