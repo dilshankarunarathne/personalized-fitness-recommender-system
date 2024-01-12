a82bfc5bc71e7f3c2eaa33ed113d33c1113a705b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-13 01:53:46 +0530

    Update bmi.py

--- backend/medic/bmi.py
+++ backend/medic/bmi.py
@@ -1,3 +1,10 @@
 """
-BMI Calculator
+    BMI Calculator
+    --------------
+    This module contains the functions to calculate the BMI of a patient.
+
+    Functions
+    ---------
+    calculate_bmi(weight, height)
+        Calculates the BMI of a patient.
 """
