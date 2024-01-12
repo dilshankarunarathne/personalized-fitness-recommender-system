993a24831e4859d49849534e1fd8cbd862a33b48
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-13 01:53:59 +0530

    Update bmi.py

--- backend/medic/bmi.py
+++ backend/medic/bmi.py
@@ -9,4 +9,19 @@
         Calculates the BMI of a patient.
 """
 
+def calculate_bmi(weight, height):
+    """Calculates the BMI of a patient.
 
+    Parameters
+    ----------
+    weight : int
+        The weight of the patient in kilograms.
+    height : int
+        The height of the patient in centimeters.
+
+    Returns
+    -------
+    float
+        The BMI of the patient.
+    """
+    return weight / ((height / 100) ** 2)
