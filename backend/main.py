e733abcc37a308d61fb0fb276165e6aff83a23a7
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-13 01:57:17 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,12 @@ from routes import main
 
 app = FastAPI()
 
-## CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
