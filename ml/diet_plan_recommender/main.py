1b0322be83681d197076a2d41070ce999b1ece58
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-13 17:35:07 +0530

    Update main.py

--- ml/diet_plan_recommender/main.py
+++ ml/diet_plan_recommender/main.py
@@ -55,6 +55,87 @@ class Recommender:
         return df_results
 
 
+class Profile:
+    df = pd.read_csv('dataset.csv')  # static variable
+
+    def __init__(self, diet, disease, Nutrient, food_type, favorite_food):
+        self.diet = diet
+        self.disease = disease
+        self.nutrient = Nutrient
+        self.type = food_type
+        self.like = favorite_food
+        self.df2 = pd.DataFrame(columns=list(Profile.df.columns))
+        self.df3 = pd.DataFrame(columns=list(Profile.df.columns))
+        self.df4 = pd.DataFrame(columns=list(Profile.df.columns))
+        self.df5 = pd.DataFrame(columns=list(Profile.df.columns))
+        self.df6 = pd.DataFrame(columns=list(Profile.df.columns))
+
+    def removestop(self, tokens):
+        stop = set(stopwords.words('english'))
+        file = open('stopwords.txt', 'r')
+        l = list(file.read().split())
+        stop = list(stop) + l
+        l = [token for token in tokens if token not in stop]
+        return l
+
+    def inputs(self, diet, disease, Nutrient, food_type, favorite_food):
+
+        if Nutrient:
+            self.df2 = Profile.df[Profile.df.Nutrient.isin(Nutrient)]
+            self.df2 = self.df2.reset_index()
+
+        if food_type:
+            self.df2 = self.df2[self.df2.Veg_Non.isin(food_type)]
+            self.df2 = self.df2.reset_index()
+
+        if diet:
+            for i in range(self.df2.shape[0]):
+                l = str(self.df2.loc[i, 'Diet']).split()
+
+                for d in diet:
+                    if d in l:
+                        self.df4 = self.df4._append(self.df2.loc[i])
+
+        if disease:
+            for i in range(self.df2.shape[0]):
+                l = str(self.df2.loc[i, 'Disease']).split()
+                for d in disease:
+                    if d in l:
+                        self.df5 = self.df5._append(self.df2.loc[i])
+
+        if favorite_food:
+            f = self.removestop(favorite_food.split())
+            for i in range(Profile.df.shape[0]):
+                n = [j.lower() for j in str(Profile.df.loc[i, 'Name']).split()]
+                for j in n:
+                    for k in f:
+                        if k == j:
+                            self.df6 = self.df6._append(Profile.df.loc[i])
+            for i in range(Profile.df.shape[0]):
+                n = [j.lower() for j in str(Profile.df.loc[i, 'description']).split()]
+                for j in n:
+                    for k in f:
+                        if k == j:
+                            self.df6 = self.df6._append(df.loc[i])
+            for i in range(Profile.df.shape[0]):
+                n = [j.lower() for j in str(Profile.df.loc[i, 'catagory']).split()]
+                for j in n:
+                    for k in f:
+                        if k == j:
+                            self.df6 = self.df6._append(Profile.df.loc[i])
+
+        return self.df2, self.df3, self.df4, self.df5, self.df6
+
+    def get_profile(self):
+        df2, df3, df4, df5, df6 = self.inputs(self.diet, self.disease, self.nutrient, self.type, self.like)
+
+        df_merge = pd.concat([df2, df3, df4, df5, df6], axis=0).drop_duplicates(subset='Name')
+        df_merge = df_merge.filter(['Name', 'Nutrient', 'Veg_Non', 'Price', 'Review', 'description'])
+        print(df_merge.shape)
+
+        return df_merge
+
+
 ob = Recommender()
 data = ob.get_features()
 
