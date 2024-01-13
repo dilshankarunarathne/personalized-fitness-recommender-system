9560551a18528a298558d4b9dd62cd36dbd674a2
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-13 17:45:53 +0530

    Update main.py

--- backend/routes/main.py
+++ backend/routes/main.py
@@ -43,7 +43,7 @@ async def root(
         diseases.append('diabeties')
     if bmi > 25:
         diseases.append('obesity')
-    meal_plan = get_meal_plan(['low_sodium_diet','low_fat_diet'], ['diabeties'], ['calcium','vitamin_c'], ['non-veg'],'i love indian')
+    meal_plan = get_meal_plan(['low_sodium_diet','low_fat_diet'], diseases, ['calcium','vitamin_c'], ['non-veg'],'i love indian')
     # ['low_sodium_diet','low_fat_diet'], ['diabeties'], ['calcium','vitamin_c'], ['non-veg'],'i love indian'
 
     return {
