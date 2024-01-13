88db492ffa665c98553684eceb3332fa0ce39079
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-13 17:37:46 +0530

    Update main.py

--- ml/diet_plan_recommender/main.py
+++ ml/diet_plan_recommender/main.py
@@ -131,12 +131,6 @@ class Profile:
 ob = Recommender()
 data = ob.get_features()
 
-total_features = data.columns
-d = dict()
-for i in total_features:
-    d[i]= 0
-# print(d)    # not needed
-
 sample_input = ['high_protien_diet', 'gluten_free_diet', 'diabeties', 'anemia', 'calcium', 'protien']
 
 for i in sample_input:
