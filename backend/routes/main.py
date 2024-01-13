0ea8fb0cadb53e04aa5ebf8a5c1785bc540d1286
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-13 16:58:32 +0530

    Update main.py

--- backend/routes/main.py
+++ backend/routes/main.py
@@ -4,7 +4,7 @@ import numpy as np
 from fastapi import APIRouter, Form, UploadFile, File
 
 from backend.blood_report_analyzer.main import analyze_blood_sugar_report
-from backend.meal_plan_recommender.need_calculator import get_dietary_plan
+from backend.meal_plan_recommender.need_calculator import get_dietary_need
 from backend.medic.main import calculate_bmi, calculate_dream_weight
 from backend.workout_routine_recommender.recommender import predict_workout_plan
 
@@ -34,7 +34,7 @@ async def root(
     dream_weight = calculate_dream_weight(weight, bmi)
     blood_sugar_level = analyze_blood_sugar_report(img)
 
-    need = get_dietary_plan(weight, height, age, gender.lower())  # 'male' 'female'
+    need = get_dietary_need(weight, height, age, gender.lower())  # 'male' 'female'
     workout_plan = predict_workout_plan(gender, age, weight, dream_weight, bmi)   # TODO gender - 'Male' 'Female'
 
     return {
