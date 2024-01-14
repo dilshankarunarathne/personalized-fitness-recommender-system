a013d093a9e848493e9420c3a85623ae54c6e4a5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-14 16:28:04 +0530

    Update diseases.py

--- backend/nutrition_need_calculator/diseases.py
+++ backend/nutrition_need_calculator/diseases.py
@@ -1,6 +1,6 @@
 def get_diseases(blood_sugar_level, bmi):
     diseases = []
-    if blood_sugar_level > 140:
+    if blood_sugar_level is not None and blood_sugar_level > 140:
         diseases.append('diabeties')
     if bmi > 25:
         diseases.append('obesity')
