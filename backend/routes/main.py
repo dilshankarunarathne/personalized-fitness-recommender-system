290337ca22740c297a3774b3e704b24f9a3aa6aa
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-14 16:27:18 +0530

    Update main.py

--- backend/routes/main.py
+++ backend/routes/main.py
@@ -36,14 +36,16 @@ async def root(
     dream_weight = calculate_dream_weight(weight, bmi)
     if img:
         blood_sugar_level = analyze_blood_sugar_report(img)
-
+        diseases = get_diseases(blood_sugar_level, bmi)
+    else:
+        diseases = get_diseases(None, bmi)
+        
     nutrition_need = get_dietary_need(weight, height, age, gender.lower())  # 'male' 'female'
     workout_plan = predict_workout_plan(gender, age, weight, dream_weight, bmi)  # TODO gender - 'Male' 'Female'
 
     if img:
         diseases = get_diseases(blood_sugar_level, bmi)
-    else:
-        diseases = get_diseases(None, bmi)
+
 
     meal_plan = get_meal_plan(['low_sodium_diet', 'low_fat_diet'], diseases, ['calcium', 'vitamin_c'], ['non-veg'],
                               'i love indian')
