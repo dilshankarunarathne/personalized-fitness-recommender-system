425ebb119de6799a68ac33fff6341fb257ce3342
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-01-14 17:23:17 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 import configparser 
 
 config = configparser.RawConfigParser()
-config.read('application.properties')
+config.read('backend/application.properties')
 
 
 def get(section: str, key: str):
